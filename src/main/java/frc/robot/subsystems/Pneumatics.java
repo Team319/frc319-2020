cc251198314c90d98789a287b5be1acff0c0dd0c
Adam Ross <jojobn745@gmail.com>
2020-01-19 13:24:33 -0500

    added pneumatics subsystem

---
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Pneumatics extends SubsystemBase {
+
+  private Compressor compressor = new Compressor(0);
+
+  private DoubleSolenoid hoodSolenoid = new DoubleSolenoid(2, 3);
+
+  /**
+   * Creates a new Pneumatics.
+   */
+  public Pneumatics() {
+
+  }
+
+  public void compressorRun() {
+    compressor.setClosedLoopControl(true);
+  }
+
+  public void hoodExtend() {
+    this.hoodSolenoid.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void hoodRetract() {
+    this.hoodSolenoid.set(DoubleSolenoid.Value.kReverse);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
