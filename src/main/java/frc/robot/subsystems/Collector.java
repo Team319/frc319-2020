0139dab0a9392bdb7ff42843eddca5fabec47e30
Adam ROss <jojobn745@gmail.com>
2020-01-21 16:32:50 -0500

    Added BobTalonSRX, as well as PID support for collector arm

--- src/main/java/frc/robot/subsystems/Collector.java
+++ src/main/java/frc/robot/subsystems/Collector.java
@@ -8,33 +8,43 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.models.BobTalonSRX;
+import frc.robot.models.MotionParameters;
+import frc.robot.models.PhoenixGains;
 
 public class Collector extends SubsystemBase {
-  private CANSparkMax collectorLead = new CANSparkMax(8, MotorType.kBrushless);
-  private TalonSRX collectorArm = new TalonSRX(9);
+  private final CANSparkMax collectorLead = new CANSparkMax(8, MotorType.kBrushless);
+  private final BobTalonSRX collectorArm = new BobTalonSRX(9);
+
+  private final PhoenixGains collectorGains = new PhoenixGains(0, 0, 0, 0, 0, 0);
+  private final MotionParameters collectorMotionParameters = new MotionParameters(0, 0, collectorGains);
+
   /**
    * Creates a new Collector.
    */
   public Collector() {
-  
+
+    this.collectorArm.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+    this.collectorArm.configClosedloopRamp(0.25);
+    this.collectorArm.configMotionParameters(collectorMotionParameters);
   }
 
-
-@Override
+  @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 
-  public void setCollector(ControlMode controlMode, double setpoint) {
+  public void setCollector(final ControlMode controlMode, final double setpoint) {
     collectorLead.set(setpoint);
   }
 
-  public void setCollectorArm(ControlMode controlMode, double setpoint) {
+  public void setCollectorArm(final ControlMode controlMode, final double setpoint) {
     collectorArm.set(controlMode, setpoint);
   }
 }
