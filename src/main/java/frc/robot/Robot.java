3c8e66f9dacecc949716c202dd322f65b50bb685
Adam <jojobn745@gmail.com>
2020-02-09 09:59:53 -0500

    Addition of serializer subsystem

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Serializer;
 import frc.robot.subsystems.Collector;
 import frc.robot.subsystems.Shooter;
 
@@ -25,6 +26,7 @@ public class Robot extends TimedRobot {
   public static Shooter shooter = new Shooter();
   public static Collector collector = new Collector();
   public static Drivetrain drivetrain = new Drivetrain();
+  public static Serializer serializer = new Serializer();
   public static OI oi;
 
   private Command m_autonomousCommand;
